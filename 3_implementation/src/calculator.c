801bcc8a7fbba858ccef86a8bcb87f742076e69f
ravi chitti <chittiravi10@gmail.com>
2022-03-03 23:10:46 +0530

    changes

--- 3_implementation/src/calculator.c
+++ 3_implementation/src/calculator.c
@@ -27,11 +27,9 @@ int multiplication(int a2,int b2)
 }
 int division(int a3,int b3)
 {
-    
-    int div;
     if(a3!=0 && b3!=0)
     {
-    div=a3/b3;
+    int div=a3/b3;
     return div;
     }
     else 
